17bb4c58f50b66d8c7a8e34780b749c0c7f815bc
Paige Tiedeman <paigetiedeman@gmail.com>
2021-11-30 11:00:46 -0800

    tests passing with actions/actiontypes

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,4 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
+import Moment from 'moment';
+import * as c from './../../actions/ActionTypes';
 
 describe('ticketListReducer', () => {
 
@@ -7,6 +9,7 @@ describe('ticketListReducer', () => {
     names: 'Ryan & Aimen',
     location: '4b',
     issue: 'Redux action is not working correctly.',
+    timeOpen : 0,
     id: 1
   };
 
@@ -55,6 +58,48 @@ describe('ticketListReducer', () => {
         location: '2a',
         issue: 'Reducer has side effects.',
         id: 2 }
+    });
+  });
+
+  test('Should add a formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes',
+      id: id
+    };
+    expect(ticketListReducer({ [id] : ticketData }, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes'
+      }
+    });
+  });
+
+  test('should successfully add a ticket to the ticket list that includes Moment-formatted wait times', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.ADD_TICKET,
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id,
+      formattedWaitTime: new Moment().fromNow(true)
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: 'a few seconds'
+      }
     });
   });
 });
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -19,6 +19,26 @@ class TicketControl extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    1000
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("component updated!");
+  }
+
+  componentWillUnmount(){
+    console.log("component unmounted!");
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime = () => {
+    console.log("tick");
+  }
+  
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
     const action = a.addTicket(newTicket);
