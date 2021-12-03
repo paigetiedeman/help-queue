e4be003bcdd37b4df8101b71e784b28b6899d8df
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-03 10:53:55 -0800

    uninstall firebase/reinstall old version

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,28 +1,28 @@
 import * as c from './../actions/ActionTypes';
 
 export default (state = {}, action) => {
-  const { names, location, issue, id } = action;
+  const { id, formattedWaitTime } = action;
   switch (action.type) {
-  case 'ADD_TICKET':
-    return Object.assign({}, state, {
-      [id]: {
-        names: names,
-        location: location,
-        issue: issue,
-        id: id
-      }
-    });
+  // case 'ADD_TICKET':
+  //   return Object.assign({}, state, {
+  //     [id]: {
+  //       names: names,
+  //       location: location,
+  //       issue: issue,
+  //       id: id
+  //     }
+  //   });
   case c.DELETE_TICKET:
-    let newState = { ...state };
+    const newState = { ...state };
     delete newState[id];
     return newState;
-  // case c.UPDATE_TIME:
-  //   const newTicket = Object.assign({}, state[id], {formattedWaitTime});
-  //   const updatedState = Object.assign({}, state, {
-  //     [id]: newTicket
-  //   });
-  //   return updatedState;
+  case c.UPDATE_TIME:
+    const updatedTicket = Object.assign({}, state[id], {formattedWaitTime});
+    const updatedState = Object.assign({}, state, {
+      [id]: updatedTicket
+    });
+    return updatedState;
   default:
     return state;
-    }
+  }
 };
